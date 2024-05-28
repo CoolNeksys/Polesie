35c12244b5ef33040d9910af4040e249df5d6dfd
CoolNeksys <103257888+CoolNeksys@users.noreply.github.com>
2024-05-28 23:19:45 +0300

    Add files via upload

---
+++ configFB.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyANpT-SYi1AlAOdPehXipzaTQ8_AqV32iE",
+  authDomain: "sport-app-zheshko.firebaseapp.com",
+  projectId: "sport-app-zheshko",
+  storageBucket: "sport-app-zheshko.appspot.com",
+  messagingSenderId: "10235753616",
+  appId: "1:10235753616:web:4f96afae83f8227755d415"
+};
+
